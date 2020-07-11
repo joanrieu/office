8a99a9faf5193c5e9020a3d81ca698113ec5d935
Joan Rieu <joan@joanrieu.com>
2020-07-11 16:58:58 -0400

    setup project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+import "minireset.css";
+import "./style.scss";
+
+const App = () => <div>hello</div>;
+
+render(<App />, document.getElementById("app"));
